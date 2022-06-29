bbbabd69741fd9fe1a4673b0f63a57fb795a5326
JPGcl <JPGutierrezOrtega@gmail.com>
2022-06-29 13:02:30 -0400

    ETL - Prueba.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+ # config.action_controller.asset_host = "https://agile-woodland-30788.herokuapp.com/"
+
+
 
 end
